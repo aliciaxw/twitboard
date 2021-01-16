841c5dd9af30f9d87f12716ba4f379778cdf7b57
aliciaxw <alicia.xinran.wang@gmail.com>
2021-01-15 22:41:03 -0500

    Remove logging

--- index.js
+++ index.js
@@ -31,9 +31,7 @@ async function main() {
     });
 
     app.get('/', async (req, res, next) => {
-        console.log('/ req.cookies', req.cookies)
         if (req.cookies && req.cookies.twitter_screen_name) {
-            console.log('/ authorized', req.cookies.twitter_screen_name)
             return res.send(TEMPLATE.replace('CONTENT', `
             <h1>Hello ${req.cookies.twitter_screen_name}</h1>
             <br>
@@ -54,16 +52,13 @@ async function main() {
     app.get('/twitter/authorize', twitter('authorize'))
     function twitter(method = 'authorize') {
         return async (req, res) => {
-            console.log(`/twitter/${method}`)
             const { oauthRequestToken, oauthRequestTokenSecret } = await getOAuthRequestToken()
-            console.log(`/twitter/${method} ->`, { oauthRequestToken, oauthRequestTokenSecret })
 
             req.session = req.session || {}
             req.session.oauthRequestToken = oauthRequestToken
             req.session.oauthRequestTokenSecret = oauthRequestTokenSecret
 
             const authorizationUrl = `https://api.twitter.com/oauth/${method}?oauth_token=${oauthRequestToken}`
-            console.log('redirecting user to ', authorizationUrl)
             res.redirect(authorizationUrl)
         }
     }
@@ -71,7 +66,6 @@ async function main() {
     app.get('/twitter/callback', async (req, res) => {
         const { oauthRequestToken, oauthRequestTokenSecret } = req.session
         const { oauth_verifier: oauthVerifier } = req.query
-        console.log('/twitter/callback', { oauthRequestToken, oauthRequestTokenSecret, oauthVerifier })
 
         const { oauthAccessToken, oauthAccessTokenSecret, results } = await getOAuthAccessTokenWith({ oauthRequestToken, oauthRequestTokenSecret, oauthVerifier })
         req.session.oauthAccessToken = oauthAccessToken
@@ -82,12 +76,14 @@ async function main() {
         req.session.twitter_screen_name = user.screen_name
         res.cookie('twitter_screen_name', user.screen_name, { maxAge: 900000, httpOnly: true })
 
-        console.log('user succesfully logged in with twitter', user.screen_name)
         req.session.save(() => res.redirect('/'))
     })
 
     app.listen(3000, () => console.log('listening on http://127.0.0.1:3000'))
 }
+
+
+/** Twitter OAuth Helper Functions **/
 
 async function oauthGetUserById(userId, { oauthAccessToken, oauthAccessTokenSecret } = {}) {
     return promisify(oauthConsumer.get.bind(oauthConsumer))(`https://api.twitter.com/1.1/users/show.json?user_id=${userId}`, oauthAccessToken, oauthAccessTokenSecret)
